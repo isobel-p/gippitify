38452d1fcba16c05f4183d3901b0b882f07ae40d
Isobel <173300585+isobel-p@users.noreply.github.com>
2026-07-06 15:09:53 +0100

    add character counter

--- gippitify/static/script.js
+++ gippitify/static/script.js
@@ -1,6 +1,8 @@
 const textarea = document.getElementById('input');
+const counter = document.getElementById('counter');
 
 textarea.addEventListener('input', function() {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight + 'px';
+    counter.textContent = textarea.value.length + " characters"
 });
